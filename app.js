4d7e7c98d52ed3acb3585976eee0a68f99485add
Vipul Daripkar <vipuldaripkar@gmail.com>
2020-08-29 10:48:14 +0530

    Added styles internally

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const http = require('http');
+const path = require('path');
 
 const express = require('express');
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use('/', (req, res, next) => {
-    res.status(404).send('<h1>Page not found!</h1>')
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 
