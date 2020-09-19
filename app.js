ec3fb65bf3578d769883fe098340f66133a8bd77
Vipul Daripkar <vipuldaripkar@gmail.com>
2020-09-19 10:49:37 +0530

    Used Pug template engine on all pages

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
@@ -18,7 +19,8 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  //res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 // const server = http.createServer(app);
