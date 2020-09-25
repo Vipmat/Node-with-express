e259a55fea7f1917db85d73e0ca164308fa65217
Vipul Daripkar <vipuldaripkar@gmail.com>
2020-09-25 14:07:25 +0100

    created controllers and models

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 // const expressHbs = require("express-handlebars");
 
+const productsController = require("./controllers/error");
+
 const app = express();
 
 // app.engine(
@@ -20,19 +22,16 @@ app.set("view engine", "ejs");
 // app.set("view engine", "pug");
 app.set("views", "views");
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  //res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
-});
+app.use(productsController.get404);
 
 // const server = http.createServer(app);
 
