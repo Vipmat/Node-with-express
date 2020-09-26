8cfcaec3c35c43ecc19535367ffa26f0bc6a1f7e
Vipul Daripkar <vipuldaripkar@gmail.com>
2020-09-26 04:29:03 +0100

    added data storing in file and refactoring

--- models/product.js
+++ models/product.js
@@ -1,4 +1,19 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+
+const rootDir = require("../utils/path");
+
+const p = path.join(rootDir, "data", "products.json");
+
+getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
   constructor(t) {
@@ -6,10 +21,15 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
 };
