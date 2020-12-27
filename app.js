e14cc7eb24854a8f61b5fb4cb37d81840e4a5df4
Vipul Daripkar <vipuldaripkar@gmail.com>
2020-12-27 10:48:14 +0530

    Refactored code

--- app.js
+++ app.js
@@ -2,24 +2,12 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-// const expressHbs = require("express-handlebars");
 
-const productsController = require("./controllers/error");
+const errorController = require("./controllers/error");
 
 const app = express();
 
-// app.engine(
-//   "hbs",
-//   expressHbs({
-//     layoutsDir: "views/layouts/",
-//     defaultLayout: "main-layout",
-//     extname: "hbs",
-//   })
-// );
-
 app.set("view engine", "ejs");
-// app.set("view engine", "hbs");
-// app.set("view engine", "pug");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
@@ -31,10 +19,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use(productsController.get404);
-
-// const server = http.createServer(app);
-
-// server.listen(3000);
+app.use(errorController.get404);
 
 app.listen(3000);
